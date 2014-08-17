708e147abd0cdd3f994e342577e8e4374c70cff2
timkg <tim.theodor@gmail.com>
2014-08-17 03:34:38 +0200

    walls

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 var mainState = {
   preload: function () {
     game.load.image('player', 'assets/player.png');
+    game.load.image('wallV',  'assets/wallVertical.png');
+    game.load.image('wallH',  'assets/wallHorizontal.png');
+  },
+
+  createWalls: function () {
+    this.walls = game.add.group();
+    this.walls.enableBody = true;
+
+    game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left
+    game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right
+    game.add.sprite(0, 0, 'wallH', 0, this.walls); // Top Left
+    game.add.sprite(300, 0, 'wallH', 0, this.walls); // Top right
+    game.add.sprite(0, 320, 'wallH', 0, this.walls); // Bottom left
+    game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
+    game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left
+    game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
+
+    var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+    middleTop.scale.setTo(1.5, 1);
+    var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+    middleBottom.scale.setTo(1.5, 1);
+
+    this.walls.setAll('body.immovable', true);
   },
 
   create: function () {
@@ -9,15 +32,17 @@ var mainState = {
 
     this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
     this.player.anchor.setTo(.5, .5);
-
     game.physics.arcade.enable(this.player);
     this.player.body.gravity.y = 500;
+
+    this.createWalls();
 
     this.cursor = game.input.keyboard.createCursorKeys();
 
   },
 
   update: function () {
+    game.physics.arcade.collide(this.player, this.walls);
     this.movePlayer();
   },
 
